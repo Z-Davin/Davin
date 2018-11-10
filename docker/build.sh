78485b425108acc4a0d3341007555737c1d95a00
洪亮 <hong.l@belle.com.cn>
2018-11-10 21:36:05 +0800

    Update docker/build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -46,9 +46,7 @@ helm_build(){
     rm -rf ./docker/$APP && mkdir ./docker/$APP
 
     cp -rf ./docker/helm/* ./docker/$APP
-
-    tree ./docker/$APP
-
+ 
     if [ ! -e "$DEPLOY_PACKAGE" ]; then
 		echo "remove deployer-deployment.yaml"
         rm -rf  ./docker/$APP/templates/deployer-deployment.yaml
@@ -73,6 +71,8 @@ EOF
 	done
 	
     cd $WORK_DIR/docker/
+
+    tree ./docker/$APP
 
     helm package --app-version $VERSION --version $VERSION.$IID ./$APP
 
